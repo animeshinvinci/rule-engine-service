85cff054ff4dd2d5bbe1d1101e62f746d91707fa
animesh <animesh@animesh.me>
2020-03-18 14:03:37 +0530

    Initial commit

---
+++ src/test/java/me/animesh/ruleengineservice/RuleEngineServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.animesh.ruleengineservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleEngineServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
